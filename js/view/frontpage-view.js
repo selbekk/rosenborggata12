76936c153b2b7e79a99d7804c87ea8e3298200ea
Kristofer Selbekk <kristofer.selbekk@bekk.no>
2013-08-27 20:34:08 +0200

    Add SlidesJS

---
+++ js/view/frontpage-view.js
@@ -0,0 +1,60 @@
+(function() {
+    var FrontPageView = function() {
+        var nav = $('.nav');
+        var navTab = $('.nav-tab');
+        var search = $('.search-wrapper');
+        var searchTab = $('.search-tab');
+        var searchField = $('.search-wrapper input');
+
+        var toggleMenu = function() {
+            searchTab.toggleClass('hidden');
+            navTab.toggleClass('hidden');
+            nav.slideToggle(200);
+        };
+
+        var toggleSearch = function() {
+            navTab.toggleClass('hidden');
+            searchTab.toggleClass('hidden');
+            search.fadeToggle(200);
+
+            if( search.is(':visible') ) {
+                searchField.focus();
+            }
+        };
+
+        var hideSearch = function() {
+            navTab.toggleClass('hidden');
+            searchTab.toggleClass('hidden');
+            search.fadeOut(200);
+        };
+
+        var hideSearchKeyboard = function(e) {
+            if(e.keyCode === 27) /* escape */ {
+                searchField.blur();
+            }
+        }
+
+        navTab.on('click', toggleMenu);
+        searchTab.on('click', toggleSearch);
+        searchField.on('blur', hideSearch);
+        searchField.on('keyup', hideSearchKeyboard);
+
+        $(".slides").slidesjs({
+            width: 600,
+            height: 200,
+            navigation: {
+                active: false
+            },
+            play: {
+                auto: true,
+                active: false
+            },
+            pagination: {
+                active: false
+            }
+        });
+    };
+
+    window.view = window.view || {};
+    window.view.frontPage = new FrontPageView();
+})();
