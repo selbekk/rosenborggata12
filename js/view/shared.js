11484e4203b647c79400287da44013f7fa910657
Kristofer Selbekk <kristofer.selbekk@bekk.no>
2013-08-30 18:09:48 +0200

    Add marker to map, remove on mobile & tablet

--- js/view/shared.js
+++ js/view/shared.js
@@ -2,14 +2,23 @@
 
     var map;
     var init = function() {
-      var mapOptions = {
-                center: new google.maps.LatLng(59.92652, 10.7256),
-                zoom: 16,
+        var myLatlng = new google.maps.LatLng(59.92684,10.725421);
+        var mapOptions = {
+                center: myLatlng,
+                zoom: 15,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
-                disableDefaultUI: true
+                panControl: false,
+                zoomControl: true,
+                scaleControl: false
             };
-      map = new google.maps.Map(document.getElementById('maps-container'),
+        map = new google.maps.Map(document.getElementById('maps-container'),
           mapOptions);
+
+        var marker = new google.maps.Marker({
+            position:   myLatlng,
+            map:        map,
+            title:      "Rosenborggata 12"
+        });
     }
 
     google.maps.event.addDomListener(window, 'load', init);
